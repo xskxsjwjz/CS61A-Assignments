925fb70f55e78e73acf5b3b69c70e6cb1be16186
xskxsjwjz <xskxsjwjz@sjtu.edu.cn>
2025-08-28 13:02:17 +0800

    finish lab09 scheme is so complicated

--- Lab/lab09/lab09.scm
+++ Lab/lab09/lab09.scm
@@ -1,10 +1,29 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) 
+  (cond 
+  ((< num1 num2) -1)
+  ((= num1 num2)  0)
+  ((> num1 num2)  1)
+  )
+)
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) 
+  (define (helper inc)
+    (+ num inc)
+  )
+  helper
+)
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) 
+  (lambda (x) (f (g x)))
+)
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (repeat f n) 
+  (lambda (x)
+    (if (= n 0)
+        x
+        ((repeat f (- n 1))(f x)))
+  )
+)
 
 (define (max a b)
   (if (> a b)
@@ -16,4 +35,8 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define (gcd a b) 
+  (define large (max a b))
+  (define small (min a b))
+  (if (= (modulo large small) 0) small (gcd (modulo large small) small))
+)
