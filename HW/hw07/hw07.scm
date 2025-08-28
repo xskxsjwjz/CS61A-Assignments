4257690bba7b2513d33efd98402d30871e422228
xskxsjwjz <xskxsjwjz@sjtu.edu.cn>
2025-08-28 18:06:16 +0800

    finish hw07

--- HW/hw07/hw07.scm
+++ HW/hw07/hw07.scm
@@ -1,15 +1,21 @@
 (define (square n) (* n n))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp) 
+  (if (= exp 0)
+      1
+      (if (= (modulo exp 2) 0)
+          (pow (* base base) (/ exp 2))
+          (* base (pow (* base base) (/ (- exp 1) 2)))))
+)
 
 (define (repeatedly-cube n x)
   (if (zero? n)
       x
-      (let (_________________)
+      (let ((y (repeatedly-cube (- n 1) x)))
         (* y y y))))
 
 (define (cddr s) (cdr (cdr s)))
 
-(define (cadr s) 'YOUR-CODE-HERE)
+(define (cadr s) (car (cdr s)))
 
-(define (caddr s) 'YOUR-CODE-HERE)
+(define (caddr s) (car (cddr s)))
