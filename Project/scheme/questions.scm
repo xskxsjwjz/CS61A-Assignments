eed94a76388e2262c580f4db589c896ff06be8de
xskxsjwjz <xskxsjwjz@sjtu.edu.cn>
2025-09-01 23:31:37 +0800

    finish essential part of Project "Scheme"

--- Project/scheme/questions.scm
+++ Project/scheme/questions.scm
@@ -7,8 +7,17 @@
 ;; Returns a list of two-element lists
 (define (enumerate s)
   ; BEGIN PROBLEM 15
-  'replace-this-line
+  (define (helper s index) 
+    (if (null? s)
+        '()
+        (cons
+          (list index (car s))
+          (helper (cdr s) (+ index 1))
+        )
+    )
   )
+  (helper s 0)
+)
   ; END PROBLEM 15
 
 ;; Problem 16
@@ -17,8 +26,20 @@
 ;; the merged lists.
 (define (merge ordered? s1 s2)
   ; BEGIN PROBLEM 16
-  'replace-this-line
+  (if (and (null? s1) (null? s2))
+      '()
+      (cond ((null? s1) s2)
+            ((null? s2) s1)
+            ((ordered? (car s1) (car s2))
+              (cons (car s1) (merge ordered? (cdr s1) s2))
+            )
+            (else
+              (cons (car s2) (merge ordered? s1 (cdr s2)))
+            )
+      )
+      
   )
+)
   ; END PROBLEM 16
 
 ;; Optional Problem 2
